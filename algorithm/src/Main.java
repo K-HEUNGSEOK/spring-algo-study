62fe6524182b576eeec5859690f66aaedf642c14
김흥석 <eovnfjfpa@naver.com>
2025-11-20 22:32:00 +0900

    알고리즘 복습용 폴더 추가

---
+++ algorithm/src/Main.java
@@ -0,0 +1,8 @@
+import java.io.FileInputStream;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("src/input.txt"));
+    
+    }
+}
