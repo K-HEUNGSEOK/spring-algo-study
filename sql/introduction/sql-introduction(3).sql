4711bcb120ad753734f82176d97d465f6f58f611
김흥석 <eovnfjfpa@naver.com>
2025-12-09 12:43:19 +0900

    feat : SQL 입문 섹션4 문제풀이

---
+++ sql/introduction/sql-introduction(3).sql
@@ -0,0 +1,40 @@
+CREATE DATABASE my_test;
+USE my_test;
+CREATE TABLE members(
+	id INT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    join_date DATE 
+);
+
+INSERT INTO members (id, name, join_date) VALUES (1,'션','2025-01-10'), (2,'네이트','2025-02-15');
+
+UPDATE members SET name = '네이트2' WHERE id = 2;
+DELETE FROM members WHERE id = 1;
+SELECT * FROM members;
+
+CREATE TABLE products(
+	product_id INT AUTO_INCREMENT PRIMARY KEY,
+    proudct_name VARCHAR(100) NOT NULL,
+    product_code VARCHAR(20) UNIQUE,
+    price INT NOT NULL,
+    stock_count INT NOT NULL DEFAULT 0
+);
+DESC products;
+
+CREATE TABLE customers(
+	customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL
+);
+CREATE TABLE orders(
+	order_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id INT NOT NULL,
+    order_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+    constraint fr_orders_customers FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+);
+INSERT INTO customers(customer_id, name)
+VALUES(1, '홍길동');
+SELECT * FROM customers;
+INSERT INTO orders(customer_id) VALUES(1);
+SELECT * FROM orders;
+
+USE my_shop;
